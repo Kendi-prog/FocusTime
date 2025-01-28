080a738a5a96346018576cff1da2d23205f466fb
Kendi <muriungikendileila@gmail.com>
2025-01-28 10:46:13 +0300

    storing the subject and adding the rounded button component

--- src/features/focus.js
+++ src/features/focus.js
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { TextInput } from "react-native-paper";
 
 import { colors } from '../utils/colors';
+import { RoundedButton } from '../components/roundedButton';
 
-export const Focus = () => (
-    <View style={styles.container}>
-        <View style={styles.inputContainer}>
-            <TextInput label='What would you like to focus on?'/>
+export const Focus = () => {
+    const [subject, setSubject] = useState(null);
+   
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                style={styles.textInput} 
+                label='What would you like to focus on?'
+                onChangeText={setSubject}/>
+            </View>
+            <View style={styles.button}>
+                <RoundedButton title='+' size={50}  />
+            </View>
         </View>
-    </View>
-)
+    );
+}
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    textInput: {
+        flex: 1,
+        marginRight: 10
+    },
+    button: {
+        justifyContent: 'center',
+    },
     inputContainer: {
-        flex: 0.5,
-        padding: 23,
-        justifyContent: 'top'
+        padding: 25,
+        justifyContent: 'top',
+        flexDirection: 'row',
     },
     text: {
         color: colors.white,
