5907d86689be945ff59f0b4b2f68ccd96f79dd15
Kendi <muriungikendileila@gmail.com>
2025-01-29 22:24:42 +0300

    adding the focus subjet and hooking up the progress bar

--- src/features/timer.js
+++ src/features/timer.js
@@ -1,25 +1,43 @@
 import React, { useState } from "react";
-import { View, StyleSheet} from "react-native";
+import { View, StyleSheet, Text} from "react-native";
+import { ProgressBar } from "react-native-paper";
 
 import { Countdown } from "../components/countDown";
 import { RoundedButton } from "../components/roundedButton";
+import { spacing } from "../utils/sizes";
+import { colors } from "../utils/colors";
 
 export const Timer = ({ focusSubject }) => {
     const [isStarted, setIsStarted] = useState(false);
+    const [progress, setProgress] = useState(1);
 
     return (
         <View style={styles.container}>
             <View style={styles.countdown}>
                 <Countdown 
                 isPaused={!isStarted}
-                onProgress={() => {}}
+                onProgress={setProgress}
                 onEnd={() => {}}/>
+
+                <View style={{ paddingTop: spacing.xxl}}>
+                    <Text style={styles.title}>Focusing on:</Text>
+                    <Text style={styles.task}>{ focusSubject }</Text>
+                </View>
+
             </View>
+
+            <View style={{ paddingTop: spacing.sm}}>
+                <ProgressBar
+                    progress={progress}
+                    color={colors.progressBar }
+                    style={{ height: spacing.sm }}
+                />
+            </View>
+            
             <View style={styles.buttonWrapper}>
-                {!isStarted && (
+                {!isStarted ? (
                     <RoundedButton title='start' onPress={() => setIsStarted(true)}/>
-                )}
-                 {!isStarted && (
+                ) : (
                     <RoundedButton title='pause' onPress={() => setIsStarted(false)}/>
                 )}
                 
@@ -44,5 +62,14 @@ const styles = StyleSheet.create({
         padding: 15,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    title: {
+        color: colors.white,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    task: {
+        color: colors.white,
+        textAlign: 'center'
     }
 });
